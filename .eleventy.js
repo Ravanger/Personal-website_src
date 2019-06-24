a95f498028f9bd4e697e89de58676f4cc651e7ad
Ravanger <Boris.Rozzy@gmail.com>
2019-06-24 19:23:52 -0400

    Log messages

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,7 @@
 module.exports = eleventyConfig => {
+    console.log("Configuring eleventy...");
     eleventyConfig.addCollection("tagList", require("./_includes/js/getTagList"));
     eleventyConfig.setDataDeepMerge(true); //Merge directory and page tags
-
-    //Prettify html output
-    let pretty = require("pretty");
-    eleventyConfig.addTransform("pretty", (content, outputPath) => {
-        if (outputPath.endsWith(".html")) {
-            return pretty(content);
-        }
-
-        return content;
-    });
 
     //Readable date
     let { DateTime } = require("luxon");
@@ -18,8 +9,20 @@ module.exports = eleventyConfig => {
         return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLLL dd, yyyy");
     });
 
+    //HTML date
     // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
     eleventyConfig.addFilter("htmlDateString", dateObj => {
         return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    });
+
+    //Prettify html output
+    console.log("Prettifying html...");
+    let pretty = require("pretty");
+    eleventyConfig.addTransform("pretty", (content, outputPath) => {
+        if (outputPath.endsWith(".html")) {
+            return pretty(content);
+        }
+
+        return content;
     });
 };
