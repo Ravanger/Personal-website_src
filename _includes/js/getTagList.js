d42f378bc133147a68a8a11f64cffaf82367b92a
Ravanger <Boris.Rozzy@gmail.com>
2019-06-29 23:36:03 -0400

    Arrow functions and formatting

--- _includes/js/getTagList.js
+++ _includes/js/getTagList.js
@@ -1,26 +1,26 @@
-module.exports = function(collection) {
-  let tagSet = new Set();
-  collection.getAll().forEach(function(item) {
-    if( "tags" in item.data ) {
-      let tags = item.data.tags;
+module.exports = collection => {
+	let tagSet = new Set();
+	collection.getAll().forEach(item => {
+		if ('tags' in item.data) {
+			let tags = item.data.tags;
 
-      tags = tags.filter(function(item) {
-        switch(item) {
-          // this list should match the `filter` list in tags.njk
-          case "pages":
-          case "projects":
-            return false;
-        }
+			tags = tags.filter(item => {
+				switch (item) {
+					// this list should match the `filter` list in tags.njk
+					case 'pages':
+					case 'projects':
+						return false;
+				}
 
-        return true;
-      });
+				return true;
+			});
 
-      for (const tag of tags) {
-        tagSet.add(tag);
-      }
-    }
-  });
+			for (const tag of tags) {
+				tagSet.add(tag);
+			}
+		}
+	});
 
-  // returning an array in addCollection works in Eleventy 0.5.3
-  return [...tagSet].sort();
+	// returning an array in addCollection works in Eleventy 0.5.3
+	return [...tagSet].sort();
 };
