3dc2bd1a078796a226acc92862e9d0ef3fd8d6a1
Ravanger <Boris.Rozzy@gmail.com>
2019-07-01 15:18:59 -0400

    Optimize images - animated gif

--- scripts/optimize_images.js
+++ scripts/optimize_images.js
@@ -1,11 +1,13 @@
 const sharp = require('sharp');
+const gifResize = require('@gumlet/gif-resize');
 const path = require('path');
 const fs = require('fs');
+const sizeOf = require('image-size');
 
 const imagesInputPath = path.join(__dirname, '../img/');
 console.log('Optimizing images from ' + imagesInputPath);
 
-//https://gist.github.com/kethinov/6658166#gistcomment-2596168
+// https://gist.github.com/kethinov/6658166#gistcomment-2596168
 const walkSync = (dir, filelist = []) => {
 	fs.readdirSync(dir).forEach(file => {
 		const dirFile = path.join(dir, file);
@@ -21,36 +23,43 @@ const walkSync = (dir, filelist = []) => {
 	return filelist;
 };
 
-const imagesArray = walkSync(imagesInputPath);
-console.log(imagesArray);
+// Create directory if doesn't already exist
+const mkdirSync = path => {
+	if (!fs.existsSync(path)) {
+		fs.mkdirSync(path);
+	}
+};
 
+const filesArray = walkSync(imagesInputPath); //All images and videos
+console.log(filesArray);
+
+// Create _site/img/ output path
 const imagesOutputPath = path.join(__dirname, '../_site/img/');
-if (!fs.existsSync(imagesOutputPath)) {
-	fs.mkdirSync(imagesOutputPath);
-}
+mkdirSync(imagesOutputPath);
 console.log('Outputting to ' + imagesOutputPath);
 
-imagesArray.forEach(currentImageInput => {
-	const currentImageOutputDir = path.join(
-		imagesOutputPath,
-		path.basename(path.dirname(currentImageInput))
-	);
-	const currentImageOutput = path.join(
-		currentImageOutputDir,
-		path.basename(currentImageInput)
-	);
+filesArray.forEach(currentImageInput => {
 	const currentFileExt = path.extname(currentImageInput);
 	switch (currentFileExt) {
 		case '.jpg':
 		case '.png':
-		case '.gif':
 			{
-				if (!fs.existsSync(currentImageOutputDir)) {
-					fs.mkdirSync(currentImageOutputDir);
-				}
+				const currentImageOutputDir = path.join(
+					imagesOutputPath,
+					path.basename(path.dirname(currentImageInput))
+				); // ../_site/img/example_dir/
+				mkdirSync(currentImageOutputDir);
+
+				const currentImageOutput = path.join(
+					currentImageOutputDir,
+					path.basename(currentImageInput)
+				); // ../_site/img/example_dir/example.jpg
+
+				// Create empty placeholder file for each image
 				fs.openSync(currentImageOutput, 'wx');
 				console.log(currentImageOutput);
 
+				// Resize to maxWidth=800px if image is wider
 				sharp(currentImageInput)
 					.resize({
 						width: 800,
@@ -60,6 +69,35 @@ imagesArray.forEach(currentImageInput => {
 					.catch(error => {
 						console.log('*** ' + error + ': ' + currentImageInput);
 					});
+			}
+			break;
+		case '.gif':
+			{
+				const currentImageOutputDir = path.join(
+					imagesOutputPath,
+					path.basename(path.dirname(currentImageInput))
+				); // ../_site/img/example_dir/
+				mkdirSync(currentImageOutputDir);
+
+				const currentImageOutput = path.join(
+					currentImageOutputDir,
+					path.basename(currentImageInput)
+				); // ../_site/img/example_dir/example.gif
+				console.log(currentImageOutput);
+
+				// Create copy of gif before optimizations
+				fs.copyFileSync(currentImageInput, currentImageOutput);
+
+				const buf = fs.readFileSync(currentImageOutput);
+				const currentImageWidth = sizeOf(currentImageOutput).width;
+				gifResize({
+					width: currentImageWidth,
+					optimizationLevel: 3
+				})(buf).then(data => {
+					var stream = fs.createWriteStream(currentImageOutput);
+					stream.write(data);
+					stream.end();
+				});
 			}
 			break;
 		default:
