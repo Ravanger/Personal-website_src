baebfd7053f9744e1cbe7310ed8249798ea8f644
Ravanger <Boris.Rozzy@gmail.com>
2020-03-02 20:47:52 -0500

    Tags page

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -2,45 +2,43 @@ import React from "react"
 import PropTypes from "prop-types"
 import kebabCase from "lodash/kebabCase"
 
-import { Link, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export const pageQuery = graphql`
-  query {
-    allMdx(limit: 2000) {
-      group(field: frontmatter___tags) {
-        fieldValue
-        totalCount
+const Tags = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx(limit: 2000) {
+        group(field: frontmatter___tags) {
+          fieldValue
+          totalCount
+        }
       }
     }
-  }
-`
+  `)
 
-const Tags = ({
-  data: {
-    allMdx: { group },
-  },
-}) => (
-  <Layout>
-    <div>
-      <SEO title="Tags" />
+  return (
+    <Layout>
       <div>
-        <h1>Tags</h1>
-        <ul>
-          {group.map(tag => (
-            <li key={tag.fieldValue}>
-              <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                {tag.fieldValue} ({tag.totalCount})
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <SEO title="Tags" />
+        <div>
+          <h1>Tags</h1>
+          <ul>
+            {data.allMdx.group.map(tag => (
+              <li key={tag.fieldValue}>
+                <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+                  {tag.fieldValue} ({tag.totalCount})
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 Tags.propTypes = {
   data: PropTypes.shape({
