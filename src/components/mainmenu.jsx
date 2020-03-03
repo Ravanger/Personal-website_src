6a7c7e9b763caba78e941645e9396ad2e7bd8a45
Ravanger <Boris.Rozzy@gmail.com>
2020-03-02 22:30:28 -0500

    Adding empty pages

--- src/components/mainmenu.jsx
+++ src/components/mainmenu.jsx
@@ -5,9 +5,9 @@ import { Link } from "gatsby"
 const MainMenu = () => (
   <nav>
     <Link to="/">Projects</Link>
-    <Link to="/">Visual</Link>
-    <Link to="/">Audio</Link>
-    <Link to="/">Contact</Link>
+    <Link to="/visual">Visual</Link>
+    <Link to="/audio">Audio</Link>
+    <Link to="/contact">Contact</Link>
   </nav>
 )
 
