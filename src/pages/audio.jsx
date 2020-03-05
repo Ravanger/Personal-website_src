63a677b3deaa8833623eda989aa6e7cdfaeeac4a
Ravanger <Boris.Rozzy@gmail.com>
2020-03-04 23:41:53 -0500

    Audio page - Soundcloud

--- src/pages/audio.jsx
+++ src/pages/audio.jsx
@@ -6,6 +6,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" description="Home is where the house is" />
+    <iframe
+      title="SoundCloud"
+      width="100%"
+      height="666"
+      scrolling="no"
+      frameBorder="no"
+      allow="autoplay"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/19542333&color=%235a9c88&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+    ></iframe>
   </Layout>
 )
 
