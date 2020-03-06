852e67e0d551a28fb60aa252b85b1c0ab3e7beea
Ravanger <Boris.Rozzy@gmail.com>
2020-03-05 19:55:11 -0500

    Testing form functionality

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,7 +5,16 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" description="Home is where the house is" />
+    <SEO title="Contact" description="Send me an email" />
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input name="name" placeholder="Your name" type="text" />
+      <button>Send</button>
+    </form>
   </Layout>
 )
 
