dc9d643d4767980c1a2454aee8e0b608dd66e6e1
Ravanger <Boris.Rozzy@gmail.com>
2020-03-06 00:08:21 -0500

    Blogpost next/prev styling + another sample post

--- src/components/templates/post.jsx
+++ src/components/templates/post.jsx
@@ -28,32 +28,36 @@ const Post = ({ data: { mdx }, pageContext }) => {
 
   return (
     <Layout>
-      <div>
-        <SEO
-          title={mdx.frontmatter.title}
-          description={mdx.frontmatter.description}
-        />
-        <h4>
-          {prev && (
+      <SEO
+        title={mdx.frontmatter.title}
+        description={mdx.frontmatter.description}
+      />
+      <div className="pure-g">
+        {prev && (
+          <div className="pure-u-1-3">
             <Link to={prev.frontmatter.path}>
               {"< " + prev.frontmatter.title}
             </Link>
-          )}
-          {next && (
+          </div>
+        )}
+        <div className="pure-u-1-3"></div>
+        {!prev && <div className="pure-u-1-3"></div>}
+        {next && (
+          <div className="pure-u-1-3">
             <Link to={next.frontmatter.path}>
               {next.frontmatter.title + " >"}
             </Link>
-          )}
-        </h4>
-        <h1>{mdx.frontmatter.title}</h1>
-        <h2>{mdx.frontmatter.date}</h2>
-        <h3>
-          {tags.map(tag => (
-            <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-          ))}
-        </h3>
-        <MDXRenderer>{mdx.body}</MDXRenderer>
+          </div>
+        )}
       </div>
+      <h1>{mdx.frontmatter.title}</h1>
+      <h2>{mdx.frontmatter.date}</h2>
+      <h3>
+        {tags.map(tag => (
+          <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+        ))}
+      </h3>
+      <MDXRenderer>{mdx.body}</MDXRenderer>
     </Layout>
   )
 }
