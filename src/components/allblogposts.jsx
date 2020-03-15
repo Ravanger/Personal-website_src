73fb7199dd5f66262ad757971651dfd9f1f1cddb
Ravanger <Boris.Rozzy@gmail.com>
2020-03-15 15:51:21 -0400

    Main nav - page counter

--- src/components/allblogposts.jsx
+++ src/components/allblogposts.jsx
@@ -5,29 +5,14 @@ import BlogNav from "./allblogposts_nav"
 
 const AllBlogPosts = ({ pageContext }) => {
   const { group, index, pageCount } = pageContext
-  const previousUrl = index - 1 === 1 ? "/" : "/" + (index - 1).toString()
-  const nextUrl = "/" + (index + 1).toString()
-
-  const isFirst = index === 1
-  const isLast = index === pageCount
 
   return (
     <div className="allblogposts">
-      <BlogNav
-        isFirst={isFirst}
-        isLast={isLast}
-        nextUrl={nextUrl}
-        previousUrl={previousUrl}
-      />
+      <BlogNav index={index} pageCount={pageCount} />
       {group.map(({ node }) => (
         <BlogPost key={node.id} post={node} />
       ))}
-      <BlogNav
-        isFirst={isFirst}
-        isLast={isLast}
-        nextUrl={nextUrl}
-        previousUrl={previousUrl}
-      />
+      <BlogNav index={index} pageCount={pageCount} />
     </div>
   )
 }
--- src/components/allblogposts_nav.jsx
+++ src/components/allblogposts_nav.jsx
@@ -8,22 +8,34 @@ const NavWrapper = styled.nav`
   margin: 0 2rem;
 `
 
+const DivCenter = styled.div`
+  text-align: center;
+`
+
 const DivNext = styled.div`
   text-align: right;
 `
 
 const AllBlogPostsNav = props => {
+  const previousUrl =
+    props.index - 1 === 1 ? "/" : "/" + (props.index - 1).toString()
+  const nextUrl = "/" + (props.index + 1).toString()
+
+  const isFirst = props.index === 1
+  const isLast = props.index === props.pageCount
+
   return (
     <NavWrapper>
-      <div className="pure-u-1-2">
-        <NavPrevNext
-          test={props.isFirst}
-          url={props.previousUrl}
-          text="< Prev"
-        />
+      <div className="pure-u-1-3">
+        <NavPrevNext test={isFirst} url={previousUrl} text="< Prev" />
       </div>
-      <DivNext className="pure-u-1-2">
-        <NavPrevNext test={props.isLast} url={props.nextUrl} text="Next >" />
+      <DivCenter className="pure-u-1-3">
+        <span>
+          Page {props.index} of {props.pageCount}
+        </span>
+      </DivCenter>
+      <DivNext className="pure-u-1-3">
+        <NavPrevNext test={isLast} url={nextUrl} text="Next >" />
       </DivNext>
     </NavWrapper>
   )
